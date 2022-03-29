4d6e4dbe8e107e9f92f81b6dc0a3a12ec674465b
cee breezy <djmisscbrown@hotmail.com>
2022-03-29 13:31:11 +0100

    Added user sessions

--- controllers/api/use-routes.js
+++ controllers/api/use-routes.js
@@ -22,8 +22,54 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
     }
 });
-//handling login request
 
-//handling log out request
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
 
-// handling sign  up request
+        if (!dbUserData) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect email or password, please try again!'});
+            return;
+        }
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect email or password, please try again!'});
+            return;
+        }
+
+        // When user logs in successfully, set up sessions variable loggedIn
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res
+            .status(200)
+            .json({ user: dbUserData, message: 'Yay! You are now logged in!'});
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+    // Destroy session when user logs out
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
