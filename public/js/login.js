b111295882de3a0e8c8ae00a534122c1cf9b5089
cee breezy <djmisscbrown@hotmail.com>
2022-03-29 20:53:49 +0100

    Added login form handler

--- public/js/login.js
+++ public/js/login.js
@@ -7,3 +7,28 @@
 // make post request
 
 // if they login correctly we redirect to /dashboard 
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Collect values from the login form
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        // Send a POST request to the API endpoint
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // If successful, redirect the browser to dashboard
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
