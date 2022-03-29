e9d3a69dc28dcea93f603b8aec47d2a62ed0de4d
cee breezy <djmisscbrown@hotmail.com>
2022-03-29 16:03:15 +0100

    Fixed issue causing bug

--- controllers/index.js
+++ controllers/index.js
@@ -5,11 +5,11 @@ const router = require('express').Router();
 const userRoutes = require('./api/user-routes');
 const homeRoutes = require('./homepage-routes');
 const dashboardRoutes = require('./dashboard-routes');
-const apiRoutes = require('./api')
+//const apiRoutes = require('./api')
 
 router.use('/', homeRoutes);
 router.use('/users', userRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/api', apiRoutes);
+//router.use('/api', apiRoutes);
 
 module.exports = router;
