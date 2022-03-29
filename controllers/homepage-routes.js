921028506fd65cc9b5bedf122739c63c0608ff11
cee breezy <djmisscbrown@hotmail.com>
2022-03-29 10:49:39 +0100

    Added dashboard and homepage routes

---
+++ controllers/homepage-routes.js
@@ -0,0 +1,14 @@
+
+//create router
+
+
+
+// get request on '/' go off and get all the posts/blogs in the db
+
+//once we get those back res.render('all-posts', posts)
+
+// login  - get /login res.render('login')
+
+
+
+//logout
