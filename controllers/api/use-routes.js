47c7fa7237108c2ffc32a3de768c046af425c42f
cee breezy <djmisscbrown@hotmail.com>
2022-03-29 13:15:34 +0100

    created new user functionality

--- controllers/api/use-routes.js
+++ controllers/api/use-routes.js
@@ -1,5 +1,27 @@
 //user-routes
 
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// Create new user
+router.post('/', (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        // Set up sessions with loggedIn set to true
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 //handling login request
 
 //handling log out request
