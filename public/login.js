2ae12754d081d2af65742a7c200064b8e914bbaf
cee breezy <djmisscbrown@hotmail.com>
2022-03-29 10:50:53 +0100

    Added login page

---
+++ public/login.js
@@ -0,0 +1,9 @@
+// this file will be scripted into login handlebars
+
+// on lick of a button on the handlebars
+
+//we get whats enntered in fields
+
+// make post request
+
+// if they login correctly we redirect to /dashboard 
