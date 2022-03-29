84ae4855be84a3e39897fc0dfa4ba0f2e203d2b2
cee breezy <djmisscbrown@hotmail.com>
2022-03-29 16:15:04 +0100

    Moved file location of login js

--- public/login.js
+++
@@ -1,9 +0,0 @@
-// this file will be scripted into login handlebars
-
-// on click of a button on the handlebars
-
-//we get whats enntered in fields
-
-// make post request
-
-// if they login correctly we redirect to /dashboard 
