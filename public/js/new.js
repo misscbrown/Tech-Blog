5b6ba636e86a37e78f1c5b1292d12f44264cc040
cee breezy <djmisscbrown@hotmail.com>
2022-03-30 10:52:51 +0100

    Added new form handler with async function

---
+++ public/js/new.js
@@ -0,0 +1,25 @@
+const newFormHandler = async function(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const body = document.querySelector('textarea[name="post-body"]').value.trim();
+
+    const token = localStorage.getItem('token');
+    await fetch (`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            body
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`
+        }
+    });
+
+    document.location.replace('/dashboard');
+};
+
+document
+    .querySelector('#new-post-form')
+    .addEventListener('submit', newFormHandler);
