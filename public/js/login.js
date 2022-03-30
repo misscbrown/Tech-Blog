542e04771994b5ca44b5b09a8996d573226a2e82
cee breezy <djmisscbrown@hotmail.com>
2022-03-30 10:53:20 +0100

    Deleted comments

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,3 @@
-// this file will be scripted into login handlebars
-
-// on click of a button on the handlebars
-
-//we get whats enntered in fields
-
-// make post request
-
-// if they login correctly we redirect to /dashboard 
-
 const loginFormHandler = async (event) => {
     event.preventDefault();
 
