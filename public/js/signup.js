ebfcf6dc42a20f61835f917aef2fa0530da35c38
cee breezy <djmisscbrown@hotmail.com>
2022-04-01 15:35:33 +0100

    Fixed bug preventing signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,42 +1,31 @@
 const signupFormHandler = async (event) => {
+  console.log('*************')
     event.preventDefault();
 
-    const name = document.querySelector('#name-signup').value.trim();
+    const username = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    console.log('here is the stuff' + name, email, password)
+    console.log('************************-------*******here is the stuff' + username, email, password)
+    console.log('*****************************************************')
 
-    fetch("/api/user", {
-        method: "post",
-        body: JSON.stringify({
-          username: name.value,
-          email: email.value,
-          password: password.value
-        }),
-        headers: { "Content-Type": "application/json" }
-      })
-        .then(function() {
-          document.location.replace("/dashboard");
-        })
-        .catch(err => console.log('hold up ' + err));
-    };
 
-//     if (name && email && password) {
-//         const response = await fetch('/api/users', {
-//             method: 'POST',
-//             body: JSON.stringify({ name, email, password }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
+    if (username && email && password) {
+        const response = await fetch('/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-//         if (response.ok) {
-//             document.location.replace('/dashboard');
-//         } else {
-//             alert('Sorry! ' + response.statusText);
-//         }
-//     }
-// };
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Sorry! ' + response.statusText);
+        }
+    }
+};
 
 document
     .querySelector('#signup-form')
     .addEventListener('submit', signupFormHandler);
+//signupFormHandler();
